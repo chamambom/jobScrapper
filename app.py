53b71c9098bde08eea0c75c7f57ec91ccbd3ad57
Martin Chamambo <Martin.Chamambo@liquidtelecom.com>
2019-07-15 16:37:39 +0200

    fixed country dropdowns to pass arguments to a flask route via POST

--- app.py
+++ app.py
@@ -5,37 +5,40 @@ import requests
 app = Flask(__name__)
 
 
+# url/jobs is your route to the below method
 @app.route('/jobs', methods=['GET', 'POST'])
 def jobs():
-    base_url = "https://ngojobsinafrica.com/?post_type=noo_job&s=&location[]=ethiopia&category[]=information-technology"
-    source = requests.get(base_url).text
-    soup = BeautifulSoup(source, 'lxml')
+    if request.method == 'POST':
+        mycountry = request.form['country']
+        base_url = "https://ngojobsinafrica.com/?post_type=noo_job&s=&location[]=" + mycountry + "&category[]=information-technology"
+        source = requests.get(base_url).text
+        soup = BeautifulSoup(source, 'lxml')
 
-    all_information_technology_jobs = soup.find_all('div', class_="loop-item-wrap")
+        all_information_technology_jobs = soup.find_all('div', class_="loop-item-wrap")
 
-    for item in all_information_technology_jobs:
-        jobs = item.find('div', class_='item-featured')
-        job_name = (jobs.a)["title"]
+        for item in all_information_technology_jobs:
+            jobs = item.find('div', class_='item-featured')
+            job_name = (jobs.a)["title"]
 
-        job_links = item.find('div', class_='item-featured')
-        job_link = (job_links.a)["href"]
+            job_links = item.find('div', class_='item-featured')
+            job_link = (job_links.a)["href"]
 
-        companies = item.find('span', class_='job-company')
-        company = (companies.a.text)
+            companies = item.find('span', class_='job-company')
+            company = (companies.a.text)
 
-        job_types = item.find('span', class_='job-type')
-        job_type = job_types.a.text
+            job_types = item.find('span', class_='job-type')
+            job_type = job_types.a.text
 
-        job_locations = item.find('span', class_='job-location')
-        job_location = job_locations.a.text
+            job_locations = item.find('span', class_='job-location')
+            job_location = job_locations.a.text
 
-        job_dates = item.find('time', class_='entry-date')
-        job_date = job_dates.find_all('span')[1].text
+            job_dates = item.find('time', class_='entry-date')
+            job_date = job_dates.find_all('span')[1].text
 
-        dict = {'JobName': job_name, 'Organisation': company, 'JobType': job_type, 'JobLocation': job_location,
-                'JobExpiry': job_date[3:], 'JobLink': job_link}
+            dict = {'JobName': job_name, 'Organisation': company, 'JobType': job_type, 'JobLocation': job_location,
+                    'JobExpiry': job_date[3:], 'JobLink': job_link}
 
-        return render_template('index.html', result=dict)
+            return render_template('index.html', result=dict)
 
 
 @app.route('/', methods=['GET'])
